c193086bdf21a7a59664de585ca4678f87db89c2
James Bachini <james@mcambridge.com>
2021-02-16 14:25:30 +0000

    cleanup

--- smartcontract/src/valholla.rs
+++ smartcontract/src/valholla.rs
@@ -12,18 +12,18 @@ use std::mem;
 use std::str::from_utf8;
 
 pub trait Serdes: Sized + BorshSerialize + BorshDeserialize {
-  fn pack(&self, dst: &mut [u8]) {
-    let encoded = self.try_to_vec().unwrap();
-    dst[..encoded.len()].copy_from_slice(&encoded);
-  }
-  fn unpack(src: &[u8]) -> Result<Self, ProgramError> {
-    Self::try_from_slice(src).map_err(|_| ProgramError::InvalidAccountData)
-  }
+	fn pack(&self, dst: &mut [u8]) {
+		let encoded = self.try_to_vec().unwrap();
+		dst[..encoded.len()].copy_from_slice(&encoded);
+	}
+	fn unpack(src: &[u8]) -> Result<Self, ProgramError> {
+		Self::try_from_slice(src).map_err(|_| ProgramError::InvalidAccountData)
+	}
 }
 
 #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
 pub struct Message {
-  pub txt: String,
+	pub txt: String,
 }
 
 impl Serdes for Message {}
@@ -31,28 +31,25 @@ impl Serdes for Message {}
 entrypoint!(entry);
 
 fn entry(
-		program_id: &Pubkey,
-		accounts: &[AccountInfo],
-		instruction_data: &[u8],
+	program_id: &Pubkey,
+	accounts: &[AccountInfo],
+	instruction_data: &[u8],
 ) -> ProgramResult {
-		// Get the account to say hello to
-		let accounts_iter = &mut accounts.iter();
-		let account = next_account_info(accounts_iter)?;
+	let accounts_iter = &mut accounts.iter();
+	let account = next_account_info(accounts_iter)?;
 
-    let mut data = account.try_borrow_mut_data()?;
-    let mut unpacked = Message::unpack(&data).expect("Failed to read data");
-		msg!("unpacked ${:?}",unpacked);
+	let mut data = account.try_borrow_mut_data()?;
+	let mut unpacked = Message::unpack(&data).expect("Failed to read data");
+	msg!("unpacked ${:?}",unpacked);
 
+	let mut memo = from_utf8(instruction_data).map_err(|err| {
+			msg!("Invalid UTF-8, from byte {}", err.valid_up_to());
+			ProgramError::InvalidInstructionData
+	})?;
+	msg!("Memo (len {})", memo.len());
 
-		let mut memo = from_utf8(instruction_data).map_err(|err| {
-        msg!("Invalid UTF-8, from byte {}", err.valid_up_to());
-        ProgramError::InvalidInstructionData
-    })?;
-    msg!("Memo (len {})", memo.len());
-
-		// line needed here to add memo to unpacked data
-		
-		unpacked.pack(&mut data);
-    
-		Ok(())
+	// line needed here to add memo to unpacked data
+	
+	unpacked.pack(&mut data);
+	Ok(())
 }
