66766613e30a98fc4cc08e1f4694a2460f9e49e3
James Bachini <james@mcambridge.com>
2021-02-17 02:30:34 +0000

    cleanup

--- smartcontract/clientside-json.js
+++ smartcontract/clientside-json.js
@@ -13,7 +13,7 @@ const appPubKey = new solanaWeb3.PublicKey(`3hRYDsk1UkjztabWnhx5U9FipQteJ1zRtgoj
 const dataLayout = BufferLayout.struct([ BufferLayout.blob(1000,'txt') ]);
 
 const pushJSON = async (jsonString) => {
-  if (jsonString.length > 996) throw new Error({'e':'jsonString length greater than 996 chars'});
+	if (jsonString.length > 996) throw new Error({'e':'jsonString length greater than 996 chars'});
 	const paddedMsg = jsonString.padEnd(1000);
 	const buffer = Buffer.from(paddedMsg, 'utf8');
 	const instruction = new solanaWeb3.TransactionInstruction({
@@ -36,21 +36,21 @@ const pushJSON = async (jsonString) => {
 const pullJSON = async () => {
 	const accountInfo = await connection.getAccountInfo(appPubKey);
 	const info = dataLayout.decode(Buffer.from(accountInfo.data));
-  const json = info.txt.toString().substr(4,1000).trim();
-  return json;
+	const json = info.txt.toString().substr(4,1000).trim();
+	return json;
 }
 
 const demo = async () => {
-  const obj = { abc: 1, def: 'yo' };
-  const jsonString = JSON.stringify(obj);
-  console.log('Uploading Data: ',jsonString);
-  const confirmationReceipt = await pushJSON(jsonString);
+	const obj = { abc: 1, def: 'yo' };
+	const jsonString = JSON.stringify(obj);
+	console.log('Uploading Data: ',jsonString);
+	const confirmationReceipt = await pushJSON(jsonString);
 	console.log('TX:',confirmationReceipt);
 	console.log('Sleep 5 secs to wait for blockchain');
-  await new Promise(r => setTimeout(r, 5000));
-  const newJSON = await pullJSON();
+	await new Promise(r => setTimeout(r, 5000));
+	const newJSON = await pullJSON();
 	console.log('Blockchain data received',newJSON);
-  const newObj = JSON.parse(newJSON);
-  console.log(`All Done! ${newObj.def}`);
+	const newObj = JSON.parse(newJSON);
+	console.log(`All Done! ${newObj.def}`);
 }
 demo();
