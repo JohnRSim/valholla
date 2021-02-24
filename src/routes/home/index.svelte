4ebcb40ed796cfddc65d15acbfac1214706af70b
John Sim <jsim@fishbowlsolutions.com>
2021-02-24 10:55:09 +0000

    add page for jim to edit

---
+++ src/routes/home/index.svelte
@@ -0,0 +1,36 @@
+<script>
+	//svelte
+	import { onDestroy, onMount } from 'svelte';
+
+	//utils
+	import { navTo } from './route.js';
+
+	let isMounted = false;
+
+	onMount(() => {
+		console.info('[/home][Mount]');
+		isMounted = true;
+			
+	});
+
+	onDestroy(() => {
+		if (process.browser) {
+			console.info('[/home][Destroy]');
+		}
+	});
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+	<title>Home:: Val-hölla</title>
+	<meta name="description" content="The perfect place to chat, invest and expand the Solana ecosystem">
+</svelte:head>
+
+{#if (isMounted)}
+<section>
+	<!-- Jim Add your magic -->
+	... Return me to Ragnarok!!! arghh..
+</section>
+{/if}
